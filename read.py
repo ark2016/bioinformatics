c02ede53a340933888cd0a01d9bbeafd49a5fbe6
Ark2016 <lebedevai2@student.bmstu.ru>
2025-09-19 15:06:25 +0300

    add: read.py from ark branch

---
+++ read.py
@@ -0,0 +1,18 @@
+from Bio import SeqIO
+import pandas as pd
+import gzip
+
+records = []
+with gzip.open(r".\data\uniprot_sprot.fasta.gz", "rt", encoding="utf-8") as file:
+    for record in SeqIO.parse(file, "fasta"):
+        records.append({
+            'id': record.id,
+            'description': record.description,
+            'sequence': str(record.seq)
+        })
+
+df = pd.DataFrame(records)
+print(f"Loaded {len(df)} records")
+print(df.head())
+df.to_csv("uniprot_data.csv", index=False)
+print("Data saved as uniprot_data.csv")
