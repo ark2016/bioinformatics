c8932ca844089a367209a190ceb4d95910f75fa3
Arkadiy <91221701+ark2016@users.noreply.github.com>
2025-09-19 13:56:54 +0300

    Optimisation (#7)
    * add: optimisation
    
    * fix main cuda
    
    * upd: bigger batches  + output for server
    
    * faster?
    
    * upd&fix
    
    * optimised
    
    * fix

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import numpy as np
 from parser import simple_parse
 from compress import compress, haos_compress_binom_gen
 from scalar_mul import scalar_compare
@@ -5,52 +6,36 @@ from scalar_mul import scalar_compare
 def compare(stra, strb):
 	if len(stra) < len(strb):
 		stra, strb = strb, stra
-	
+
 	delta = len(stra)-len(strb)+1
-	
+
 	stra = compress(stra, delta, haos_compress_binom_gen(delta))
-	#print(stra, strb)
 	return scalar_compare(stra, strb)
 
 def string2arr(string):
-	return list(map(lambda x: ord(x) - ord('A'), string))
+	"""Конвертирует строку в массив чисел (A=0, B=1, ...)"""
+	return np.array([ord(c) - ord('A') for c in string], dtype=np.int32)
 
-def main(filename, test_string):
-	# Парсим файл
-	strings, names = simple_parse(filename)
-	
-	strings = strings[:50000]
-	
-	# Определяем разницу для препросчета коэфициентов свертки. Пока не используется
-	"""
-	mindelta = abs(len(test_string)-len(strings[0]))
-	maxdelta = mindelta
-	for i in range(1, len(strings)):
-		delta = abs(len(test_string)-len(strings[1]))
-		if delta > maxdelta:
-			maxdelta = delta
-		if delta < mindelta:
-			mindelta = delta
-	"""
-	
-	test_arr = string2arr(test_string)
-	
-	result = [0]*len(strings)
-	# основной цикл
-	for index in range(len(strings)):
-		if index%10 == 0: # вместо tqdm чтоб не нагружать проц
-			print(index, "/", len(strings), end="\r")
-		result[index] = compare(string2arr(strings[index]), test_arr)
-	
-	
-	indexed_list = [(index, value) for index, value in enumerate(result)]
-	# Сортируем по значению в убывающем порядке
-	sorted_list = sorted(indexed_list, key=lambda x: x[1], reverse=True)    
-	for i in range(10):
-		print(i+1, sorted_list[i], names[sorted_list[i][0]])
+def batch_compare(strings_batch, test_arr, compress_funcs):
+	"""Векторизованное сравнение батча строк с тестовой строкой"""
+	batch_size = len(strings_batch)
+	results = np.zeros(batch_size)
 
+	for i, string_arr in enumerate(strings_batch):
+		if len(string_arr) < len(test_arr):
+			string_arr, test_arr_local = test_arr, string_arr
+		else:
+			string_arr, test_arr_local = string_arr, test_arr
 
-if __name__=="__main__":
-	main("uniprot_sprot.fasta", "MALWMRLLPLLALLALWGPDPAAAFVNQHLCGSHLVEALYLVCGERGFFYTPKTRREAEDLQVGQVELGGGPGAGSLQPLALEGSLQKRGIVEQCCTSICSLYQLENYCN")
-	#main("uniprot-choline_esterase_reviewed-yes.fasta", "MHSKVTIICIRFLFWFLLLCMLIGKSHTEDDIIIATKNGKVRGMNLTVFGGTVTAFLGIPYAQPPLGRLRFKKPQSLTKWSDIWNATKYANSCCQNIDQSFPGFHGSEMWNPNTDLSEDCLYLNVWIPAPKPKNATVLIWIYGGGFQTGTSSLHVYDGKFLARVERVIVVSMNYRVGALGFLALPGNPEAPGNMGLFDQQLALQWVQKNIAAFGGNPKSVTLFGESAGAASVSLHLLSPGSHSLFTRAILQSGSFNAPWAVTSLYEARNRTLNLAKLTGCSRENETEIIKCLRNKDPQEILLNEAFVVPYGTPLSVNFGPTVDGDFLTDMPDILLELGQFKKTQILVGVNKDEGTAFLVYGAPGFSKDNNSIITRKEFQEGLKIFFPGVSEFGKESILFHYTDWVDDQRPENYREALGDVVGDYNFICPALEFTKKFSEWGNNAFFYYFEHRSSKLPWPEWMGVMHGYEIEFVFGLPLERRDNYTKAEEILSRSIVKRWANFAKYGNPNETQNNSTSWPVFKSTEQKYLTLNTESTRIMTKLRAQQCRFWTSFFPKVLEMTGNIDEAEWEWKAGFHRWNNYMMDWKNQFNDYTSKKESCVGL")
-	#main("insulin.fasta", "MALWMRLLPLLALLALWGPDPAAAFVNQHLCGSHLVEALYLVCGERGFFYTPKTRREAEDLQVGQVELGGGPGAGSLQPLALEGSLQKRGIVEQCCTSICSLYQLENYCN")
+		delta = len(string_arr) - len(test_arr_local) + 1
+
+		if delta in compress_funcs:
+			compress_func = compress_funcs[delta]
+		else:
+			compress_func = haos_compress_binom_gen(delta)
+			compress_funcs[delta] = compress_func
+
+		compressed = compress(string_arr, delta, compress_func)
+		results[i] = scalar_compare(compressed, test_arr_local)
+
+	return results
